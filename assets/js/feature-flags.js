0568794d3630a5d8bb29e20f983bbaf94ed88e5a
deca <179514251+deca-txt@users.noreply.github.com>
2026-09-23 01:01:07 -0300

    feat: define inactive future feature flags

---
+++ assets/js/feature-flags.js
@@ -0,0 +1,7 @@
+// Future product capabilities stay explicitly off until their dependencies are approved.
+window.LDX_FEATURE_FLAGS = Object.freeze({
+  leadCapture: false,
+  aiAdvisor: false,
+  cases: false,
+  analytics: false
+});
